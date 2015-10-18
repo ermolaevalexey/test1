9940b0cfac6a41790711b1fde0a16736af72b9bc
Alexey Ermolaev <descendended@gmail.com>
2015-10-18 21:37:51 +0300

    test

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="description" content=""/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Test</title>
+        <link rel="stylesheet" href="css/style.css"/>
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script src="js/js.js"></script>
+    </head>
+    <body>
+    	<header class="header">
+    			<div class="logo">
+    				 <a href="#">logo</a>
+    			</div>
+    			<nav class="main-menu">
+    			        <a class="nav_link" href="#">home</a>
+						<a class="nav_link" href="#">about</a>
+						<a class="nav_link" href="#">work</a>
+						<a class="nav_link" href="#">services</a>
+						<a class="nav_link" href="#">blog</a>
+						<form class="search">
+							<input type="text" name="search"/>
+							<button type="submit"></button>
+						</form>
+    			</nav>
+    	</header>
+    	<section class="main"></section>
+    	<section class="form"></section>
+    </body>
+</html>
