0894c1b9e2f594f52d5cb24fe565b46f8f90bcef
ermolaevalexey <descendended@gmail.com>
2015-10-19 18:07:21 +0300

    test_ver2

--- index.php
+++ index.php
@@ -11,23 +11,70 @@
 		<script src="js/js.js"></script>
     </head>
     <body>
-    	<header class="header">
-    			<div class="logo">
-    				 <a href="#">logo</a>
-    			</div>
-    			<nav class="main-menu">
-    			        <a class="nav_link" href="#">home</a>
-						<a class="nav_link" href="#">about</a>
-						<a class="nav_link" href="#">work</a>
-						<a class="nav_link" href="#">services</a>
-						<a class="nav_link" href="#">blog</a>
-						<form class="search">
-							<input type="text" name="search"/>
-							<button type="submit"></button>
-						</form>
-    			</nav>
-    	</header>
-    	<section class="main"></section>
-    	<section class="form"></section>
+    <section class="con con1">
+            <div class="int head">
+                 <header class="header">
+                         <div class="logo">
+                              <a href="#">logo</a>
+                         </div>
+                         <nav class="main-menu">
+                                 <a class="nav_link" href="#">home</a>
+                                 <a class="nav_link" href="#">about</a>
+                                 <a class="nav_link" href="#">work</a>
+                                 <a class="nav_link" href="#">services</a>
+                                 <a class="nav_link" href="#">blog</a>
+                                 <form class="search">
+                                     <input type="text" name="search"/>
+                                     <button type="submit">
+                                             <svg id="loop" width="20px" height="20px" fill="#eaeaea" viewBox="16 16 50 50">
+                                                  <path d="M59.446,55.911L49.337,45.802c1.922-2.64,3.069-5.88,3.069-9.396c0-8.837-7.163-16-16-16s-16,7.163-16,16s7.163,16,16,16  c3.516,0,6.756-1.147,9.396-3.069l10.109,10.109L59.446,55.911z M36.406,47.406c-6.065,0-11-4.935-11-11s4.935-11,11-11  s11,4.935,11,11S42.471,47.406,36.406,47.406z"/>
+                                             </svg>                   
+                                     </button>
+                                 </form>
+                                 <a class="email" href="mailto:descendended@gmail.com">
+                                     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/" x="0px" y="0px" width="15px" height="15px" viewBox="-52 -40 50 50" overflow="visible" enable-background="new -2.891 -8.901 60 60" xml:space="preserve">
+                                         <path id="rect4208_1_" fill="#FFFFFF" d="M4.75,0h44.609c2.589,0,4.75,2.163,4.75,4.751v31.597c0,2.589-2.161,4.75-4.75,4.75H4.75
+                                           c-2.589,0-4.75-2.161-4.75-4.75V4.751C0,2.163,2.161,0,4.75,0z M7.612,4.977l19.445,20.127L46.59,4.977H7.612z M4.977,7.365v25.907
+                                           l12.73-12.73L4.977,7.365z M49.133,7.461l-12.8,13.188l12.8,12.8V7.461z M20.177,23.098L7.152,36.122h39.627L33.857,23.2
+                                           l-5.527,5.695c-0.683,0.705-1.808,0.723-2.513,0.039c-0.014-0.014-0.027-0.027-0.041-0.041L20.177,23.098z"></path>
+                                     </svg>
+                                 </a>
+                         </nav>
+                    </header>
+            </div>
+    </section>
+    <section class="con main">
+             <div class="int">
+                  <div class="main">
+                       <div class="sect">
+                            <div class="icon">
+                                
+                            </div>
+                            <h2>branding</h2>
+                            <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p>
+                      </div>
+                      <div class="sect active">
+                           <div class="icon">
+                               
+                           </div>
+                           <h2>design</h2>
+                           <p>Slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.</p>
+                      </div>
+                      <div class="sect">
+                           <div class="icon">
+                               
+                           </div>
+                           <h2>development</h2>
+                           <p>Gregor Samsa woke from troubled dreams, he found himself armour-like back, and if he lifted his head a little he could see his brown belly.</p>
+                      </div>
+                  </div>
+              </div>
+                  
+    </section>
+    <section class="con content">
+             <div class="con form">
+                 
+             </div>
+    </section>
     </body>
 </html>
